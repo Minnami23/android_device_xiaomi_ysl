23db94726b6c6994124feffc16f5bd40ea20a69e
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    vince: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     camera.msm8953
 
+PRODUCT_PACKAGES += \
+    liblzma.vendor:64
+
 # Input
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/)
