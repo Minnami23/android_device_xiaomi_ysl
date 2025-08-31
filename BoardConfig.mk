a60dd3c734cf65c74deeed70a13325d6f2df5dd8
mizukanaAlt <rina301087@gmail.com>
2025-08-31 19:53:12 +0000

    vince: use prebuilt kernel
    Co-authored-by: RennAlt <shenprjktplayground@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,12 +8,14 @@
 include device/xiaomi/mititanium-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/vince
+TITANIUM_PREBUILT := device/xiaomi/titanium-prebuilt
 
 # Display
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/vince_defconfig
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_PREBUILT_KERNEL := $(TITANIUM_PREBUILT)/kernel/vince/Image.gz-dtb
 
 # Filesystem
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
