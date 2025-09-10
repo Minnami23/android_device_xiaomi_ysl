76f617db24f4d403e95f941b60fac159e1b8df86
0xKANARIA <enashinonome264@gmail.com>
2025-09-10 05:43:57 +0000

    vince: fingerprint: use libhidlbase-v32

--- device.mk
+++ device.mk
@@ -59,6 +59,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.xiaomi.device.rc
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_ODM)/lib64/libhidlbase-v32.so
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
