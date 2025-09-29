f88dc1b13f99abc4bc3d48a8fb1cd1fa7d23edbd
KanadeAlt <enashinonome264@gmail.com>
2025-09-29 13:31:29 -0400

    ysl: drop hardware/xiaomi

--- device.mk
+++ device.mk
@@ -51,8 +51,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
-    hardware/xiaomi
+    $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/ysl/ysl-vendor.mk)
